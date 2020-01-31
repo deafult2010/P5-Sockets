346ccf451bbe7760e78e0d6861a2baf22beb8bcd
deafult2010 <thomas.jaeger@blueyonder.co.uk>
2020-01-31 14:06:02 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,1 @@
+console.log('my socket server is running');
---
+++ sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+  createCanvas(400, 400);
+  frameRate(24);
+}
+
+function draw() {
+  background(0);
+  ellipse(200, 200, 50, 50);
+}
