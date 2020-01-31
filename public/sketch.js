5adc1c88bf1b383572a5838cc7efb3f4242af225
deafult2010 <thomas.jaeger@blueyonder.co.uk>
2020-01-31 15:24:39 +0000

    2nd commit

---
+++ public/sketch.js
@@ -0,0 +1,34 @@
+var socket;
+
+function setup() {
+  createCanvas(400, 400);
+  background(51);
+  frameRate(24);
+
+  socket = io.connect('http://localhost:3000');
+  socket.on('mouse', newDrawing);
+}
+
+function newDrawing(data) {
+  noStroke();
+  fill(0, 255, 0);
+  ellipse(data.x, data.y, 50, 50);
+
+  noStroke();
+  fill(255, 0, 100);
+  ellipse(data.x, data.y, 30, 30);
+}
+
+function mouseDragged() {
+  console.log('Sending: ' + mouseX + ',' + mouseY);
+
+  var data = {
+    x: mouseX,
+    y: mouseY
+  };
+  socket.emit('mouse', data);
+
+  noStroke();
+  fill(255);
+  ellipse(mouseX, mouseY, 50, 50);
+}
--- sketch.js
+++
@@ -1,9 +0,0 @@
-function setup() {
-  createCanvas(400, 400);
-  frameRate(24);
-}
-
-function draw() {
-  background(0);
-  ellipse(200, 200, 50, 50);
-}
